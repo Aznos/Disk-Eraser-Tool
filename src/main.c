3a506c3e0c7c51aa24c4b1c99f263afe1687623c
Aznos <aznosreal@gmail.com>
2024-06-20 23:29:23 -0400

    Bold

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
     printf(CLEAR_TERMINAL);
     printf("%s%s!!! WARNING !!!\n", RED, REDB);
-    printf("%sUsing this tool will %sPERMANENTLY DELETE %sall the contents in a disk and make them %sUNRECOVERABLE%s\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n", TRANSPARENTB, HRED, RED, HRED, RED);
+    printf("%sUsing this tool will %sPERMANENTLY DELETE %sall the contents in a disk and make them %sUNRECOVERABLE%s\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n", TRANSPARENTB, BHRED, RED, BHRED, RED);
 
     return 0;
 }
