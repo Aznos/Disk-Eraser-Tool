f41a8294bd0181487677366c357db3dfc5eea79e
Aznos <aznosreal@gmail.com>
2024-06-20 23:13:53 -0400

    Warning message

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("THIS IS A WARNING MESSAGE\n");
+    printf("!!! WARNING !!!\n");
+    printf("Using this tool will PERMANENTLY DELETE all the contents in a disk and make them UNRECOVERABLE\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n");
+    
     return 0;
 }
