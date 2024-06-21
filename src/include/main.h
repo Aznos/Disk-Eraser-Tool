e8f2a344e001a50eb468c0387f1b704d0248a9da
Aznos <aznosreal@gmail.com>
2024-06-21 11:25:07 -0400

    Trying to get disks

--- src/include/main.h
+++ src/include/main.h
@@ -1,14 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define DISKS 3
+#define MAX_DISKS 16
 
 struct DISK_INFO {
     unsigned int size;
 };
 
 typedef struct {
-    void (*getDisks)(struct DISK_INFO[]);
+    void (*getDisks)(struct DISK_INFO[], int*);
 } DiskManager;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,16 +1,56 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "include/colors.h"
 #include "include/main.h"
 
-void getDisks(struct DISK_INFO disks[]) {
-    disks[0].size = 512;
-    disks[1].size = 1024;
-    disks[2].size = 20480000;
+void getDisks(struct DISK_INFO disks[], int* numDisks) {
+    FILE *fp;
+    char path[1035];
+    int diskCount = 0;
+
+    fp = popen("diskutil list", "r");
+    if(fp == NULL) {
+        printf("%sFailed to run, make sure you're running on OSX", RED);
+        exit(1);
+    }
+
+    while(fgets(path, sizeof(path) - 1, fp) != NULL) {
+        if(strstr(path, "dev/disk") != NULL && diskCount < MAX_DISKS) {
+            bool sizeFound = false;
+            while(fgets(path, sizeof(path) - 1, fp) != NULL && !sizeFound) {
+                if(strstr(path, "GB") != NULL) {
+                    float sizeGB;
+                    sscanf(path, " * %f GB", &sizeGB);
+                    disks[diskCount].size = (unsigned int)(sizeGB * 1024); //Convert GB to MB
+                    sizeFound = true;
+                } else if(strstr(path, "TB") != NULL) {
+                    float sizeTB;
+                    sscanf(path, " * %f TB", &sizeTB);
+                    disks[diskCount].size = (unsigned int)(sizeTB * 1024 * 1024); //Convert TB to MB
+                } else if(strstr(path, "MB") != NULL) {
+                    float sizeMB;
+                    sscanf(path, " * %f MB", &sizeMB);
+                    disks[diskCount].size = (unsigned int)sizeMB;
+                    sizeFound = true;
+                }
+            }
+
+            diskCount++;
+        }
+    }
+
+    pclose(fp);
+    *numDisks = diskCount;
 }
 
 int main(int argc, char** argv) {
     DiskManager diskManager = {getDisks};
+    struct DISK_INFO disks[MAX_DISKS];
+    int numDisks;
+
+    diskManager.getDisks(disks, &numDisks);
     
     printf(CLEAR_TERMINAL);
     printf("%s%s!!! WARNING !!!\n", BBLACK, REDB);
@@ -28,16 +68,13 @@ int main(int argc, char** argv) {
         input[strcspn(input, "\n")] = 0;
         if(strcmp(input, "I am sure") == 0) {
             printf("%sWhich disk do you want to wipe (Or type \"all\" to wipe all disks):\n", YELLOW);
-            struct DISK_INFO disks[DISKS];
-            diskManager.getDisks(disks);
-            for(int i = 0; i < DISKS; i++) {
-                unsigned int diskNum = i + 1;
+            for(int i = 0; i < numDisks; i++) {
                 if(disks[i].size >= 1024 * 1024) {
-                    printf("%sDisk Number: %s%u\n%sDisk Size: %s%u TB\n\n", CYAN, BCYAN, diskNum, CYAN, BCYAN, disks[i].size / (1024 * 1024));
+                    printf("%sDisk Number: %s%d\n%sDisk Size: %s%u TB\n\n", CYAN, BCYAN, i + 1, CYAN, BCYAN, disks[i].size / (1024 * 1024));
                 } else if(disks[i].size >= 1024) {
-                    printf("%sDisk Number: %s%u\n%sDisk Size: %s%u GB\n\n", CYAN, BCYAN, diskNum, CYAN, BCYAN, disks[i].size / 1024);
+                    printf("%sDisk Number: %s%d\n%sDisk Size: %s%u GB\n\n", CYAN, BCYAN, i + 1, CYAN, BCYAN, disks[i].size / 1024);
                 } else {
-                    printf("%sDisk Number: %s%u\n%sDisk Size: %s%u MB\n\n", CYAN, BCYAN, diskNum, CYAN, BCYAN, disks[i].size);
+                    printf("%sDisk Number: %s%d\n%sDisk Size: %s%u MB\n\n", CYAN, BCYAN, i + 1, CYAN, BCYAN, disks[i].size);
                 }
             }
         } else {
