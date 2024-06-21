0de692d0a680b36d9bf77feff0ccb463b53f526a
Aznos <aznosreal@gmail.com>
2024-06-20 23:31:59 -0400

    Input

--- src/main.c
+++ src/main.c
@@ -4,7 +4,10 @@
 int main(int argc, char** argv) {
     printf(CLEAR_TERMINAL);
     printf("%s%s!!! WARNING !!!\n", RED, REDB);
-    printf("%sUsing this tool will %sPERMANENTLY DELETE %sall the contents in a disk and make them %sUNRECOVERABLE%s\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n", TRANSPARENTB, BHRED, RED, BHRED, RED);
+    printf("%sUsing this tool will %sPERMANENTLY DELETE %sall the contents in a disk and make them %sUNRECOVERABLE%s\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n\n%s> ", TRANSPARENTB, BHRED, RED, BHRED, RED, WHITE);
+
+    char input[12];
+    scanf("%s", input);
 
     return 0;
 }
