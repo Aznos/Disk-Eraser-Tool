a8ed8c12965ea1388483445a05a5b0df690d0642
Aznos <aznosreal@gmail.com>
2024-06-20 23:47:02 -0400

    Input

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "include/colors.h"
 
 int main(int argc, char** argv) {
@@ -6,8 +7,20 @@ int main(int argc, char** argv) {
     printf("%s%s!!! WARNING !!!\n", RED, REDB);
     printf("%sUsing this tool will %sPERMANENTLY DELETE %sall the contents in a disk and make them %sUNRECOVERABLE%s\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n\n%s> ", TRANSPARENTB, BHRED, RED, BHRED, RED, WHITE);
 
-    char input[12];
-    scanf("%s", input);
+    char input[50];
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+
+    if(strcmp(input, "I understand") == 0) {
+        printf("%s%s!!! WARNING !!!\n", RED, REDB);
+        printf("%sAre you sure you want to proceed? This action is %sIRREVERSIBLE%s\nIf you are sure you want to proceed, type \"I am sure\" and press Enter\n\n%s> ", TRANSPARENTB, BHRED, RED, WHITE);
+
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
+        if(strcmp(input, "I am sure") == 0) {
+            printf("Placeholder\n");
+        }
+    }
 
     return 0;
 }
