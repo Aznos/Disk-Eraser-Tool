50cf09109c90916772a1bd99132cf08ac06d38ca
Aznos <aznosreal@gmail.com>
2024-06-21 11:05:29 -0400

    getDisks function

--- src/include/main.h
+++ src/include/main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define DISKS 3
+
 struct DISK_INFO {
     unsigned int size;
+    unsigned int number;
 };
 
-
+typedef struct {
+    void (*getDisks)(struct DISK_INFO[]);
+} DiskManager;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include "include/colors.h"
+#include "include/main.h"
+
+void getDisks(struct DISK_INFO disks[]) {
+    disks[0].size = 512;
+    disks[0].number = 1;
+
+    disks[1].size = 1000;
+    disks[1].number = 2;
+
+    disks[2].size = 2000;
+    disks[2].number = 3;
+}
 
 int main(int argc, char** argv) {
+    DiskManager diskManager = {getDisks};
+    
     printf(CLEAR_TERMINAL);
-    printf("%s%s!!! WARNING !!!\n", RED, REDB);
-    printf("%sUsing this tool will %sPERMANENTLY DELETE %sall the contents in a disk and make them %sUNRECOVERABLE%s\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n\n%s> ", TRANSPARENTB, BHRED, RED, BHRED, RED, WHITE);
+    printf("%s%s!!! WARNING !!!\n", BBLACK, REDB);
+    printf("%s%sUsing this tool will %sPERMANENTLY DELETE %sall the contents in a disk and make them %sUNRECOVERABLE%s\nIf you are sure you want to proceed, type \"I understand\" and press Enter\n\n%s> ", TRANSPARENTB, RED, BHRED, RED, BHRED, RED, WHITE);
 
     char input[50];
     fgets(input, sizeof(input), stdin);
@@ -18,9 +32,12 @@ int main(int argc, char** argv) {
         fgets(input, sizeof(input), stdin);
         input[strcspn(input, "\n")] = 0;
         if(strcmp(input, "I am sure") == 0) {
-            printf("%sWhich disk do you want to wipe:\n", YELLOW, WHITE);
-            
-            //getDisks();
+            printf("%sWhich disk do you want to wipe (Or type \"all\" to wipe all disks):\n", YELLOW);
+            struct DISK_INFO disks[DISKS];
+            diskManager.getDisks(disks);
+            for(int i = 0; i < DISKS; i++) {
+                printf("%sDisk Number: %s%u\n%sDisk Size: %s%uMB\n\n", CYAN, BCYAN, disks[i].number, CYAN, BCYAN, disks[i].size);
+            }
         } else {
             printf("%s\nQuitting program..\n", YELLOW);
         }
