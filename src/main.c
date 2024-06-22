814bac3156d967b2833b94692e8d550b38893a8d
Aznos <aznosreal@gmail.com>
2024-06-22 13:51:59 -0400

    Updated 0's

--- src/main.c
+++ src/main.c
@@ -113,7 +113,7 @@ void eraseDisk(struct DISK_INFO disk, int num) {
     snprintf(diskPath, sizeof(diskPath), "/dev/disk%d", num);
 
     for(int i = 0; i < 10; i++) {
-        if(i < 5) {
+        if(i < 1) {
             overwriteDisk(diskPath, false, disk.size);
         } else {
             overwriteDisk(diskPath, true, disk.size);
