4d340227b338d272a5ea55f0e71716df9b732c9d
Aznos <aznosreal@gmail.com>
2024-06-23 19:16:20 -0400

    Rectangle

--- src/gui.c
+++ src/gui.c
@@ -5,11 +5,17 @@
 const int SCREENW = 1000;
 const int SCREENH = 600;
 
+void drawDiskRect(SDL_Renderer* renderer, int x, int y, int w, int h) {
+    SDL_Rect rect = {x, y, w, h};
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &rect);
+}
+
 void initGUI() {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Disk Erase Tool", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENW, SCREENH, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 50, 54, 55, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
@@ -21,6 +27,9 @@ void initGUI() {
                 running = 0;
             }
         }
+
+        drawDiskRect(renderer, 100, 100, 100, 100);
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
