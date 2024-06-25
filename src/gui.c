d8e8468a3dc0f17c654080d12afd9812146f6545
Aznos <aznosreal@gmail.com>
2024-06-24 23:14:31 -0400

    Null pointer Text

--- src/gui.c
+++ src/gui.c
@@ -20,7 +20,23 @@ SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* file) {
     return texture;
 }
 
-void drawGrid(SDL_Renderer* renderer, int numDisks, int rectW, int rectH, SDL_Color color, SDL_Texture* texture) {
+SDL_Texture* renderText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Color color) {
+    SDL_Surface* surface = TTF_RenderText_Blended(font, text, color);
+    if(surface == NULL) {
+        printf("Failed to render text: %s\n", TTF_GetError());
+        return NULL;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    if(texture == NULL) {
+        printf("Failed to create texture from surface: %s\n", SDL_GetError());
+    }
+
+    return texture;
+}
+
+void drawGrid(SDL_Renderer* renderer, int numDisks, int rectW, int rectH, SDL_Color color, SDL_Texture* texture, TTF_Font* font) {
     if(numDisks > MAX_DISKS) {
         numDisks = MAX_DISKS;
     }
@@ -43,6 +59,17 @@ void drawGrid(SDL_Renderer* renderer, int numDisks, int rectW, int rectH, SDL_Co
 
         drawDiskRect(renderer, x, y, rectW, rectH, color);
 
+        char label[20];
+        snprintf(label, sizeof(label), "Disk %d", i + 1);
+        SDL_Texture* textTexture = renderText(renderer, font, label, color);
+        if(textTexture != NULL) {
+            int textW, textH;
+            SDL_QueryTexture(textTexture, NULL, NULL, &textW, &textH);
+            SDL_Rect textRect = {x + (rectW - rectW) / 2, y - textH - 5, textW, textH};
+            SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+            SDL_DestroyTexture(textTexture);
+        }
+
         if(texture != NULL) {
             int textureW, textureH;
             SDL_QueryTexture(texture, NULL, NULL, &textureW, &textureH);
@@ -55,6 +82,7 @@ void drawGrid(SDL_Renderer* renderer, int numDisks, int rectW, int rectH, SDL_Co
 void initGUI() {
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
+    TTF_Init();
     SDL_Window* window = SDL_CreateWindow("Disk Erase Tool", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENW, SCREENH, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(renderer, 50, 54, 55, 255);
@@ -63,6 +91,7 @@ void initGUI() {
 
     SDL_Color rectColor = {255, 255, 255, 255};
     SDL_Texture* texture = loadTexture(renderer, "./src/assets/img/hdd (Nikita Golubev).png");
+    TTF_Font* font = TTF_OpenFont("./src/assets/fonts/Roboto-Regular.ttf", 24);
 
     SDL_Event event;
     int running = 1;
@@ -81,7 +110,7 @@ void initGUI() {
         SDL_SetRenderDrawColor(renderer, 50, 54, 55, 255);
         SDL_RenderClear(renderer);
 
-        drawGrid(renderer, numDisks, rectW, rectH, rectColor, texture);
+        drawGrid(renderer, numDisks, rectW, rectH, rectColor, texture, font);
 
         SDL_RenderPresent(renderer);
 
--- src/include/gui.h
+++ src/include/gui.h
@@ -7,10 +7,12 @@
 
 #include "SDL.h"
 #include "SDL_image.h"
+#include "SDL_ttf.h"
 
 void initGUI();
 void drawDiskRect(SDL_Renderer* renderer, int x, int , int w, int h, SDL_Color color);
-void drawGrid(SDL_Renderer* renderer, int numDisks, int rectW, int rectH, SDL_Color color, SDL_Texture* texture);
+void drawGrid(SDL_Renderer* renderer, int numDisks, int rectW, int rectH, SDL_Color color, SDL_Texture* texture, TTF_Font* font);
 SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* file);
+SDL_Texture* renderText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Color color);
 
 #endif
